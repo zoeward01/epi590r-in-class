8e3b56a988e90c226779b2d7c1389434d2434aa7
Louisa Smith <louisahsmith@gmail.com>
2023-08-13 11:59:20 -0400

    gtsummary examples

---
+++ R/gtsummary-examples.R
@@ -0,0 +1,142 @@
+library(tidyverse)
+library(gtsummary)
+
+nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+							 "id", "nsibs", "samp", "race_eth", "sex", "region",
+							 "income", "res_1980", "res_2002", "age_bir")
+nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols) |>
+	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+				 sex_cat = factor(sex, labels = c("Male", "Female")),
+				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+
+
+# Customization of `tbl_summary()`
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(sex_cat, race_eth_cat, region_cat,
+							eyesight_cat, glasses, age_bir))
+
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(sex_cat, race_eth_cat, region_cat,
+							eyesight_cat, glasses, age_bir),
+	label = list(
+		race_eth_cat ~ "Race/ethnicity",
+		region_cat ~ "Region",
+		eyesight_cat ~ "Eyesight",
+		glasses ~ "Wears glasses",
+		age_bir ~ "Age at first birth"
+	),
+	missing_text = "Missing")
+
+
+tbl_summary(
+	nlsy,
+	by = sex_cat,
+	include = c(sex_cat, race_eth_cat,
+							eyesight_cat, glasses, age_bir),
+	label = list(
+		race_eth_cat ~ "Race/ethnicity",
+		eyesight_cat ~ "Eyesight",
+		glasses ~ "Wears glasses",
+		age_bir ~ "Age at first birth"
+	),
+	missing_text = "Missing") |>
+	add_p(test = list(all_continuous() ~ "t.test",
+										all_categorical() ~ "chisq.test")) |>
+	add_overall(col_label = "**Total**") |>
+	bold_labels() |>
+	modify_footnote(update = everything() ~ NA) |>
+	modify_header(label = "**Variable**", p.value = "**P**")
+
+
+# Univariate regression
+
+tbl_uvregression(
+	nlsy,
+	y = income,
+	include = c(sex_cat, race_eth_cat,
+							eyesight_cat, income, age_bir),
+	method = lm)
+
+
+tbl_uvregression(
+	nlsy,
+	y = glasses,
+	include = c(sex_cat, race_eth_cat,
+							eyesight_cat, glasses, age_bir),
+	method = glm,
+	method.args = list(family = binomial()),
+	exponentiate = TRUE)
+
+
+## Multivariable regressions
+
+## Some regressions
+
+linear_model <- lm(income ~ sex_cat + age_bir + race_eth_cat,
+									 data = nlsy)
+
+
+linear_model_int <- lm(income ~ sex_cat*age_bir + race_eth_cat,
+											 data = nlsy)
+
+
+logistic_model <- glm(glasses ~ eyesight_cat + sex_cat + income,
+											data = nlsy, family = binomial())
+
+
+## Tables
+
+tbl_regression(
+	linear_model,
+	intercept = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		race_eth_cat ~ "Race/ethnicity",
+		age_bir ~ "Age at first birth"
+	))
+
+
+tbl_regression(
+	logistic_model,
+	exponentiate = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		eyesight_cat ~ "Eyesight",
+		income ~ "Income"
+	))
+
+
+tbl_no_int <- tbl_regression(
+	linear_model,
+	intercept = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		race_eth_cat ~ "Race/ethnicity",
+		age_bir ~ "Age at first birth"
+	))
+
+tbl_int <- tbl_regression(
+	linear_model_int,
+	intercept = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		race_eth_cat ~ "Race/ethnicity",
+		age_bir ~ "Age at first birth",
+		`sex_cat:age_bir` ~ "Sex/age interaction"
+	))
+
+## Table comparing the models with and without interaction
+
+tbl_merge(list(tbl_no_int, tbl_int),
+					tab_spanner = c("**Model 1**", "**Model 2**"))
+
