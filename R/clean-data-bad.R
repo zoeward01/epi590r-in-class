1dcfdafa2baba2cf37762c016d55589463d4445d
Louisa Smith <louisahsmith@gmail.com>
2023-08-14 11:25:40 -0400

    comment my code

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,8 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+# this doesn't work because we don't all have the same directory
+setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
