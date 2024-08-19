5a300855948ee33270713a4eadeb5e3f85363244
Zoe Ward <zoraw2101@gmail.com>
2024-08-19 11:30:27 -0400

    added my working directory

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,7 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
+setwd("~/EPI590R in-class/data/raw")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
@@ -18,5 +18,5 @@ nlsy <- nlsy |>
 
 nlsy <- na.omit(nlsy)
 
-setwd("../clean/")
+setwd("~/EPI590R in-class/clean")
 write_rds(nlsy, "nlsy-complete-cases.rds")
